29f9a79f07876c692d5f5a8ea8ed26c7202d3d44
danielhmills <daniel.h.mills@gmx.de>
2023-03-07 00:49:46 -0500

    latest restructure

--- advanced/load_data.sql
+++ advanced/load_data.sql
@@ -1,8 +1,3 @@
--- Ontology Cleanup + Loading
-
-SPARQL CLEAR GRAPH <https://raw.githubusercontent.com/danielhmills/virtuoso_search_recruitment_demo/main/ontology.ttl>;
-SPARQL LOAD <https://raw.githubusercontent.com/danielhmills/virtuoso_search_recruitment_demo/main/ontology.ttl>;
-
 -- SQL Table Creation + Data Entry
 -- Load Applicant Info
 
